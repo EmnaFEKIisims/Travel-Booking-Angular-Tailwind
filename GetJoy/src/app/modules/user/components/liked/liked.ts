c3f205eba14a3e6489eb4802f3dd216005f631ef
EmnaFEKIisims <emna.feki@isimsf.u-sfax.tn>
2025-11-29 19:23:41 +0100

    configurate structure

---
+++ GetJoy/src/app/modules/user/components/liked/liked.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liked',
+  imports: [],
+  templateUrl: './liked.html',
+  styleUrl: './liked.css',
+})
+export class Liked {
+
+}
